357b9d816b0d9314fc05612b0769ad8d8f7da492
BogdanCerbu <cerbub@gmail.com>
2020-06-26 04:15:32 +0300

    Add main comparing function

--- exercitii.py
+++ exercitii.py
@@ -5,10 +5,10 @@ def roll_dice(attacking_army, defending_army):
     red_dice = 0
     white_dice = 0
     if attacking_army == 1:
-        red_dice = 1
-    elif attacking_army == 2:
-        red_dice = 2
-    elif attacking_army > 2:
+        red_dice = 1                            # for x in range(3):
+    elif attacking_army == 2:                    # if attacking_army == x:
+        red_dice = 2                            # red_dice = x
+    elif attacking_army > 2:                    #
         red_dice = 3
     if defending_army == 1:
         white_dice = 1
@@ -16,15 +16,18 @@ def roll_dice(attacking_army, defending_army):
         white_dice = 2
     roll_red = sorted([random.randrange(1, 6) for x in range(0, red_dice)])
     roll_white = sorted([random.randrange(1, 6) for x in range(0, white_dice)])
-    print('\nAttacking player rolled: ' + "".join(roll_dice))
-    print('\nDefending player rolled: ' + "".join(roll_white))
-    if white_dice == 1:  # If there is only one white die, only match up the highest red die with the white die
-        if roll_white[0] >= roll_red[0]:
-            print('Defender wins! Attacker loses 1 army')
+    print('\nAttacking player rolled: ',roll_red)
+    print('\nDefending player rolled: ',roll_white)
+    for i in range(0, len(roll_white)):
+        if roll_white[i] >= roll_red[i]:
+            print('Defender wins! Defender loses 1 army')
             attacking_army -= 1
         else: 
             print('Attacker wins! Defender loses 1 army')
             defending_army -= 1
+    return (attacking_army, defending_army)
 
+
+print(roll_dice(1, 2))
 
 # print(sorted(roll_red))
