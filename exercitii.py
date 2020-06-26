cea03180f101b607310c26b8e5ef0c06ea7f2e8f
BogdanCerbu <cerbub@gmail.com>
2020-06-26 04:34:15 +0300

    Make sorting for one defending dice

--- exercitii.py
+++ exercitii.py
@@ -14,20 +14,27 @@ def roll_dice(attacking_army, defending_army):
         white_dice = 1
     elif defending_army > 1:  # maximum of 2 dices for defender
         white_dice = 2
-    roll_red = sorted([random.randrange(1, 6) for x in range(0, red_dice)])
-    roll_white = sorted([random.randrange(1, 6) for x in range(0, white_dice)])
-    print('\nAttacking player rolled: ',roll_red)
-    print('\nDefending player rolled: ',roll_white)
-    for i in range(0, len(roll_white)):
-        if roll_white[i] >= roll_red[i]:
-            print('Defender wins! Defender loses 1 army')
+    roll_red = [random.randrange(1, 6) for x in range(0, red_dice)]
+    roll_white = [random.randrange(1, 6) for x in range(0, white_dice)]
+    roll_red = sorted(roll_red)
+    roll_white = sorted(roll_white)
+    print('\nAttacking player rolled: ', roll_red)
+    print('\nDefending player rolled: ', roll_white)
+    if white_dice == 1:
+        if roll_white[0] >= roll_red[len(roll_red)-1]:
+            print('Defender wins! Attacker loses 1 army')
             attacking_army -= 1
-        else: 
+        else:
+            print('Attacker wins! Defender loses 1 army')
+            defending_army -= 1
+    for i in range(1, len(roll_white)):
+        if roll_white[i] >= roll_red[i]:
+            print('Defender wins! Attacker loses 1 army')
+            attacking_army -= 1
+        else:
             print('Attacker wins! Defender loses 1 army')
             defending_army -= 1
     return (attacking_army, defending_army)
 
 
-print(roll_dice(1, 2))
-
-# print(sorted(roll_red))
+print(roll_dice(3, 1))
