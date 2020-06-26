c18d2a42d83c20547e51fdbb11e82c12b1069d7f
BogdanCerbu <cerbub@gmail.com>
2020-06-26 21:14:38 +0300

    Clean the code

--- ideabank.py
+++ ideabank.py
@@ -2,16 +2,9 @@
 
 
 def main():
-    x = input('What is yout newn idea: ')
-
-    # f = open("ideabank.txt", "w+")
-    # for i in range(3):
-    #     f.write("This is line %d\n\r" % (i+1))
-
+    x = input('%d. What is yout newn idea? \r\n' % (1))
     f = open('ideabank.txt', 'a+')
-    i = 1
-    f.write(x)
-    i += 1
+    f.write('\n'+x)
 
     f.close()
 
